8f4d553ddfa0f8a4e93b64a3ffca2ad597be4a5d
tranbomm08 <tranbomm08@users.noreply.github.com>
2026-09-19 15:06:15 +0000

    deploy: tranbomm08/hannest-source@4a0b511b795d2c24906a9a9416f89e5cde899dcd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1693200814" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "672451512" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
